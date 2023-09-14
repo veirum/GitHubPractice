f051186fa5db53592b791f42a6f6657c20460b39
Michal Kowalski <kowal@outlook.dk>
2023-09-14 14:43:58 +0200

    gdgfd

--- GitPractice/sketch.js
+++ GitPractice/sketch.js
@@ -1,7 +1,13 @@
+let x = 0
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 400);
+  background(0);
 }
 
 function draw() {
-  background(220);
+  x+=10
+  background(0,20);
+  fill(255)
+  noStroke()
+  ellipse(x%width,200,100,100);
 }
